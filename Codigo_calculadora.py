af2b5bf279c06a5fadab654b7c7ca774ef7f2643
Heytor-Ferreira <heytorferreira.comercial@gmail.com>
2022-12-12 20:03:46 -0300

    Update Codigo_calculadora.py

--- Codigo_calculadora.py
+++ Codigo_calculadora.py
@@ -1,25 +1,20 @@
 from PyQt5 import  uic,QtWidgets
-
 def Calculo():
-
     if GUI_calculadora.radioButton.isChecked():
         Valor_1 = int(GUI_calculadora.lineEdit.text())
         Valor_2 = int(GUI_calculadora.lineEdit_2.text())
         Resultado = Valor_1+Valor_2
-        GUI_calculadora.textBrowser.setText(str(Resultado))
-        
+        GUI_calculadora.textBrowser.setText(str(Resultado))       
     elif GUI_calculadora.radioButton_2.isChecked():
         Valor_1 = int(GUI_calculadora.lineEdit.text())
         Valor_2 = int(GUI_calculadora.lineEdit_2.text())
         Resultado = Valor_1-Valor_2
-        GUI_calculadora.textBrowser.setText(str(Resultado))
-        
+        GUI_calculadora.textBrowser.setText(str(Resultado)) 
     elif GUI_calculadora.radioButton_3.isChecked():
         Valor_1 = int(GUI_calculadora.lineEdit.text())
         Valor_2 = int(GUI_calculadora.lineEdit_2.text())
         Resultado = Valor_1/Valor_2
         GUI_calculadora.textBrowser.setText(str(Resultado))
-        
     elif GUI_calculadora.radioButton_4.isChecked():
         Valor_1 = int(GUI_calculadora.lineEdit.text())
         Valor_2 = int(GUI_calculadora.lineEdit_2.text())
@@ -27,7 +22,6 @@ def Calculo():
         GUI_calculadora.textBrowser.setText(str(Resultado))
     else :
         GUI_calculadora.textBrowser.setText("ERRO")
-
 app=QtWidgets.QApplication([])
 GUI_calculadora=uic.loadUi("GUI_calculadora.ui")
 GUI_calculadora.pushButton.clicked.connect(Calculo)
